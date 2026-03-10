363184a64e804abff2b3132615f069259a26b3f7
Kuzey Edes-Huyal <151467488+f-kuzey-edes-huyal@users.noreply.github.com>
2026-03-10 18:37:45 +0300

    Add Weather Station Java source files

---
+++ observer_pattern/WeatherStation/StatisticsDisplay.java
@@ -0,0 +1,34 @@
+public class StatisticsDisplay implements Observer, DisplayElement {
+    private float maxTemp = Float.MIN_VALUE;
+    private float minTemp = Float.MAX_VALUE;
+    private float tempSum = 0.0f;
+    private int numReadings = 0;
+    private Subject weatherData;
+
+    public StatisticsDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void update(float temperature, float humidity, float pressure) {
+        tempSum += temperature;
+        numReadings++;
+
+        if (temperature > maxTemp) {
+            maxTemp = temperature;
+        }
+
+        if (temperature < minTemp) {
+            minTemp = temperature;
+        }
+
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Avg/Max/Min temperature = "
+                + (tempSum / numReadings) + "/" + maxTemp + "/" + minTemp);
+    }
+}
